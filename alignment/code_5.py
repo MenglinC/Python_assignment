d4477696e9907e1f19a1fef6208191cdd60ab9cc
MenglinC <55335232+MenglinC@users.noreply.github.com>
2022-10-08 21:56:13 +0800

    Update code_5.py

--- alignment/code_5.py
+++ alignment/code_5.py
@@ -11,8 +11,10 @@ def create_substitution_matrix(alphabet,match,mismatch):
 				submat[alphabet[i]+alphabet[j]] = mismatch
 	return submat
 
+def gapcost():
+	
 
-def smith_waterman(s1,s2,submat,gap=-1):
+def smith_waterman(s1,s2,submat,gap=-1,gap_ex=-2):
 	n = len(s1)+1
 	m = len(s2)+1
 	matrix = np.zeros((n,m))
@@ -23,12 +25,23 @@ def smith_waterman(s1,s2,submat,gap=-1):
 				# score1 = matrix[i,j]+match
 			# else:
 				# score1 = matrix[i,j]+mismatch
-			score1 = matrix[i,j]+submat[a1+a2] #就这里改了一下
-			score2 = matrix[i,j+1] + gap
-			score3 = matrix[i+1,j] + gap
-			score = np.max([score1,score2,score3,0])
+			if traceback[i,j]==0:
+				score1 = matrix[i,j]+submat[a1+a2] #就这里改了一下
+				score2 = matrix[i,j+1] + gap
+				score3 = matrix[i+1,j] + gap
+				score = np.max([score1,score2,score3,0])
+			if traceback[i,j]==1:
+				score1 = matrix[i,j]+submat[a1+a2] #就这里改了一下
+				score2 = matrix[i,j+1] + gap_ex
+				score3 = matrix[i+1,j] + gap
+				score = np.max([score1,score2,score3,0])
+		        if traceback[i,j]==2:
+				score1 = matrix[i,j]+submat[a1+a2] #就这里改了一下
+				score2 = matrix[i,j+1] + gap
+				score3 = matrix[i+1,j] + gap_ex
+				score = np.max([score1,score2,score3,0])
 			matrix[i+1,j+1] = score
-			direction = np.argmax([score1,score2,score3])
+			direction = np.argmax([score1,score2,score3]) 
 			traceback[i+1,j+1] = direction
 	optimal_score = np.max(matrix)
 	return (optimal_score,matrix,traceback)
